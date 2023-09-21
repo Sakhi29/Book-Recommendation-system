7ad9a1b3a61585e19f5615f965ba7482382e7e87
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2023-09-22 02:18:08 +0530

    Added routes

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 import pickle
 
 popular_df = pickle.load(open('popular.pkl','rb'))
+pt = pickle.load(open('pt.pkl','rb'))
+books = pickle.load(open('books.pkl','rb'))
+similarity_scores = pickle.load(open('similarity_scores.pkl','rb'))
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -12,6 +16,15 @@ def index():
                            image = list(popular_df['Image-URL-M'].values),
                            votes = list(popular_df['num_rating'].values),
                            rating = list(popular_df['avg_rating'].values))
+
+@app.route('/recommend')
+def recommend_ui():
+    return render_template('recommend.html')
+
+@app.route('/recommend_books',methods=['post'])
+def recommend():
+    user_input = request.form.get('user_input')
+    return str(user_input)
 
 
 if __name__ == '__main__':
